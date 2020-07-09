488eb0afec63011fbd4ca94d1a4d243611189cfc
calpt <36051308+calpt@users.noreply.github.com>
2020-07-09 12:55:04 +0200

    base_model -> model in adapter_training

--- src/transformers/adapter_training.py
+++ src/transformers/adapter_training.py
@@ -34,25 +34,23 @@ def setup_task_adapter_training(model, task_name: str, adapter_args: AdapterArgu
     """
     language = adapter_args.load_lang_adapter
     if adapter_args.train_adapter:
-        # get actual model for derived models with heads
-        base_model = getattr(model, model.base_model_prefix, model)
         # task adapter - only add if not existing
-        if task_name not in base_model.config.adapters.adapter_list(AdapterType.text_task):
-            base_model.set_adapter_config(AdapterType.text_task, adapter_args.adapter_config)
+        if task_name not in model.config.adapters.adapter_list(AdapterType.text_task):
+            model.set_adapter_config(AdapterType.text_task, adapter_args.adapter_config)
             # load a pre-trained adapter for fine-tuning if specified
             if adapter_args.load_task_adapter:
-                base_model.load_adapter(adapter_args.load_task_adapter, AdapterType.text_task, load_as=task_name)
+                model.load_adapter(adapter_args.load_task_adapter, AdapterType.text_task, load_as=task_name)
             # otherwise, add a new adapter
             else:
-                base_model.add_adapter(task_name, AdapterType.text_task)
+                model.add_adapter(task_name, AdapterType.text_task)
         # language adapter - only add if not existing
-        if language and language not in base_model.config.adapters.adapter_list(AdapterType.text_lang):
-            base_model.set_adapter_config(
+        if language and language not in model.config.adapters.adapter_list(AdapterType.text_lang):
+            model.set_adapter_config(
                 AdapterType.text_lang, adapter_args.lang_adapter_config or adapter_args.adapter_config
             )
-            base_model.load_adapter(language, AdapterType.text_lang)
+            model.load_adapter(language, AdapterType.text_lang)
         # enable adapter training
-        base_model.train_task_adapter()
+        model.train_task_adapter()
     # set adapters as default if possible
     if isinstance(model, BertModelHeadsMixin):
         if language:
